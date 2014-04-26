145788480d908e2f9360e367693e4fed8b248d85
Lenin Raj Rajasekaran <lrajasekaran@groupon.com>
2014-04-26 12:18:13 +0530

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TamilvuWiki::Application.config.secret_key_base = '03fbdb3f8a1ca3afd2b55f899c031beacd64eb49f0642bd49305b256f73edd9556f9f5981cd7ce6f30d43d1aca0bab38d37403ac5000fc92b40a0bc4236deeb9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TamilvuWiki::Application.config.session_store :cookie_store, key: '_tamilvu-wiki_session'
